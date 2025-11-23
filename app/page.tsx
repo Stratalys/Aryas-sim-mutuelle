d68e671e218794b95b55aada6dd0f3a1844ccf85
Anthony Damprobe <Anthony@users-air.home>
2025-11-23 12:28:17 +0100

    Mise a jour design et layout resultats

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,8 @@ import SimulateurForm from '@/components/SimulateurForm';
 
 export default function Home() {
   return (
-    <main className="min-h-screen py-8 px-4">
+    <main className="min-h-screen pt-8 px-4">
       <div className="max-w-7xl mx-auto">
-        <div className="text-center mb-8">
-          <h1 className="text-4xl font-nikkei font-bold text-bleu-cobalt mb-4">
-            Simulateur de Remboursement
-          </h1>
-          <p className="text-lg font-open text-bleu-turquin">
-            Estimez en quelques clics votre reste à charge.
-          </p>
-        </div>
         <SimulateurForm />
       </div>
     </main>
